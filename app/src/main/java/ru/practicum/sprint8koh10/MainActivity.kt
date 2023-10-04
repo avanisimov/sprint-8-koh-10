4d78c3ceed6ea9115678be604c77ecbdc015e887
Aleksei Anisimov <suxelshock@gmail.com>
2023-10-04 20:01:03 +0300

    Toolbar+ImageViews examples

--- app/src/main/java/ru/practicum/sprint8koh10/MainActivity.kt
+++ app/src/main/java/ru/practicum/sprint8koh10/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.practicum.sprint8koh10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        findViewById<ImageView>(R.id.arrow1).setOnClickListener {
+
+        }
+        findViewById<ImageView>(R.id.arrow2).setOnClickListener {
+
+        }
     }
 }
