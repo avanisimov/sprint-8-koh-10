e8e210c77da0f2c53f1433e7b54c641ece466602
Aleksei Anisimov <suxelshock@gmail.com>
2023-10-05 10:29:11 +0300

    Activity lifecycle experiments

--- app/src/main/java/ru/practicum/sprint8koh10/MainActivity.kt
+++ app/src/main/java/ru/practicum/sprint8koh10/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.practicum.sprint8koh10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    val initTime = System.currentTimeMillis()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,6 +18,32 @@ class MainActivity : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+        Log.d("SPRINT_8", "onCreate $this ${System.currentTimeMillis() - initTime} ms")
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d("SPRINT_8", "onStart $this ${System.currentTimeMillis() - initTime} ms")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SPRINT_8", "onResume $this ${System.currentTimeMillis() - initTime} ms")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SPRINT_8", "onPause $this ${System.currentTimeMillis() - initTime} ms")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("SPRINT_8", "onStop $this ${System.currentTimeMillis() - initTime} ms")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SPRINT_8", "onDestroy $this ${System.currentTimeMillis() - initTime} ms")
     }
 }
